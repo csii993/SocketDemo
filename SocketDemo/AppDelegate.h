7082bbceffd7b05b67457984c915e79748c8eebd
wxd24 <csiivip@163.com>
2019-05-05 16:43:13 +0800

    Initial Commit

---
+++ SocketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocketDemo
+//
+//  Created by wangxiaodong on 05/05/2019.
+//  Copyright © 2019 WXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketDemo
+//
+//  Created by wangxiaodong on 05/05/2019.
+//  Copyright © 2019 WXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
